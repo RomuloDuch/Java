89dfd29e711bd96974bd184366a79841797527b4
Rômulo Duch <134861603+RomuloDuch@users.noreply.github.com>
2023-05-28 13:24:45 -0300

    Add files via upload

---
+++ P1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jack Cooper
+ *
+ */
+module P1 {
+}
---
+++ P1/src/prototype1/prototipe.java
@@ -0,0 +1,9 @@
+package prototype1;
+
+public class prototipe {
+public static void main(String[] args) {
+	System.out.println("Hello world");
+	
+	
+}
+}
